ecad2491c76025f11f4867dd7fac89de92bb4d57
Arka Talukdar <arka20597@gmail.com>
2017-02-27 14:00:46 +0530

    mnist in py added

---
+++ neural-networks_MNIST/src/driver.py
@@ -0,0 +1,15 @@
+import mnist_loader
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+
+import network
+
+#constructor
+
+net = network.Network([784, 30, 10])
+
+
+# learning rate =3.0, 
+
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
